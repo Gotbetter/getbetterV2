1cb5c3339a2cac3f66e652fc567a33336ff05b81
yongun2 <djkye0001@naver.com>
2023-10-08 23:56:09 +0900

    [feat] 홈 화면 헤더, 스터디룸 리스트, 푸터

---
+++ components/home/StudyRoomStatus.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement, useMemo } from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+type PropTypes = {
+  startDate: string;
+  week: number;
+};
+
+const StudyRoomStatus = ({ startDate, week }: PropTypes): ReactElement => {
+  const roomStatus = useMemo(() => {
+    // 현재 시각
+    const today = new Date();
+    const currentTime = today.getTime() - today.getTimezoneOffset() * 60000;
+
+    // 시작일
+    const start = new Date(startDate);
+
+    if (currentTime < start.getTime()) return 'WAIT';
+
+    const endDate = new Date(startDate);
+    endDate.setDate(start.getDate() + 7 * week);
+
+    if (currentTime <= endDate.getTime()) return 'PROGRESS';
+    else return 'END';
+  }, [startDate, week]);
+
+  switch (roomStatus) {
+    case 'PROGRESS':
+      return (
+        <Container>
+          <Label color={'#3333FF'}>&#183; 진행중</Label>
+        </Container>
+      );
+
+    case 'END':
+      return (
+        <Container>
+          <Label color={'#FF3395'}>&#183; 종료</Label>
+        </Container>
+      );
+    default:
+      return <></>;
+  }
+};
+
+const Container = styled.View`
+  flex-direction: row;
+  flex-grow: 1;
+  margin-left: ${RFValue(4)}px;
+`;
+
+type LabelProps = {
+  color?: string;
+};
+
+const Label = styled.Text<LabelProps>`
+  color: ${({ color }) => color};
+  font-weight: 700;
+  font-size: ${RFValue(10)}px;
+`;
+
+export default StudyRoomStatus;
