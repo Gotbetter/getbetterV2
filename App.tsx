b9f1d7a705b9c9bf610510ed0a107b0373fc8c09
yongun2 <djkye0001@naver.com>
2023-10-08 23:58:10 +0900

    [feat] react-query, recoil 프로바이더 설정

--- App.tsx
+++ App.tsx
@@ -3,19 +3,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthRoutes from '@routes/AuthRoutes';
 import HomeRoutes from '@routes/HomeRoutes';
 import AutoLoginManager from '@utils/AutoLoginManager';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 import { RootStackParamList } from 'types/rotes';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <AutoLoginManager>
-        <Stack.Navigator initialRouteName={'AuthRoutes'} screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="AuthRoutes" component={AuthRoutes} />
-          <Stack.Screen name="HomeRoutes" component={HomeRoutes} />
-        </Stack.Navigator>
-      </AutoLoginManager>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <NavigationContainer>
+          <AutoLoginManager>
+            <Stack.Navigator initialRouteName={'AuthRoutes'} screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="AuthRoutes" component={AuthRoutes} />
+              <Stack.Screen name="HomeRoutes" component={HomeRoutes} />
+            </Stack.Navigator>
+          </AutoLoginManager>
+        </NavigationContainer>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
