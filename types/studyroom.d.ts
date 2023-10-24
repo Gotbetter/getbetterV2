9ccbd85614a393a228c71da33788a9a81868c616
yongun2 <djkye0001@naver.com>
2023-10-24 13:49:33 +0900

    [DOC] 스터디룸 및 계획 관련 타입

--- types/studyroom.d.ts
+++ types/studyroom.d.ts
@@ -10,4 +10,28 @@ type StudyRoomListType = {
   current_user_num: number;
 };
 
-export { StudyRoomListType };
+type StudyRoomDetailsType = {
+  room_id: number;
+  title: string;
+  description: string;
+  week: number;
+  room_category: string;
+  start_date: string;
+  entry_fee: number;
+  max_user_num: number;
+  current_user_num: number;
+};
+
+type StudyRoomCreateResponseType = StudyRoomDetailsType & {
+  participant_id: number;
+};
+
+type StudyRoomRuleType = {
+  order: number;
+  rule_attribute1: string;
+  rule_attribute2: string;
+  rule_code: string;
+  rule_description: string;
+};
+
+export { StudyRoomListType, StudyRoomRuleType, StudyRoomDetailsType, StudyRoomCreateResponseType };
